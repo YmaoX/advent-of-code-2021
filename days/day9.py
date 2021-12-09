5fed39978f90eea013a20b6883d816548a3ff5bf
Yuemao Xu (yuemxu) <yuemxu@cisco.com>
2021-12-09 17:57:01 +0100

    day9: part2

--- days/day9.py
+++ days/day9.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def get_input():
     matrix = []
     with open("input/day9.txt") as fo:
@@ -28,7 +31,7 @@ def find_lower(matrix: list[list], callback):
 def part1():
     lows = []
 
-    def lower_collector(matrix: list[list], i: int, j:int, value: int):
+    def lower_collector(matrix: list[list], i: int, j: int, value: int):
         lows.append(value)
 
     find_lower(get_input(), lower_collector)
@@ -36,3 +39,42 @@ def part1():
     return sum(lows) + len(lows)
 
 
+def part2():
+    ss = []
+
+    def flow(matrix: list[list], i: int, j: int, value: int):
+        acc = []
+        flow_rec(matrix, i, j, value, acc, set())
+        ss.append(len(acc))
+
+    def flow_rec(matrix: list[list], i: int, j: int, value: int, acc: list, visited: set):
+        visited.add((i, j))
+        adjacent = []
+        # if i > 0 and matrix[i - 1][j] > value:
+        #     adjacent.append((i-1, j, matrix[i - 1][j]))
+        # if i < len(matrix) - 1 and matrix[i + 1][j] > value:
+        #     adjacent.append((i+1, j, matrix[i + 1][j]))
+        # if j > 0 and matrix[i][j - 1] > value:
+        #     adjacent.append((i, j-1, matrix[i][j - 1]))
+        # if j < len(matrix[i]) - 1 and matrix[i][j + 1] > value:
+        #     adjacent.append((i, j+1, matrix[i][j + 1]))
+        if i > 0 and matrix[i - 1][j] != 9:
+            adjacent.append((i-1, j, matrix[i - 1][j]))
+        if i < len(matrix) - 1 and matrix[i + 1][j] != 9:
+            adjacent.append((i+1, j, matrix[i + 1][j]))
+        if j > 0 and matrix[i][j - 1] != 9:
+            adjacent.append((i, j-1, matrix[i][j - 1]))
+        if j < len(matrix[i]) - 1 and matrix[i][j + 1] != 9:
+            adjacent.append((i, j+1, matrix[i][j + 1]))
+
+        if len(adjacent) > 0:
+            acc.append(value)
+
+        for (x, y, v) in adjacent:
+            if not visited.__contains__((x, y)):
+                flow_rec(matrix, x, y, v, acc, visited)
+
+    find_lower(get_input(), flow)
+    ss.sort(reverse=True)
+    print(ss)
+    return ss[0] * ss[1] * ss[2]
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ from functools import reduce
 from days.day9 import *
 
 if __name__ == "__main__":
-    print(part1())
+    # 109, 96, 95
+    # (29, 47) - 0
+    print(part2())
